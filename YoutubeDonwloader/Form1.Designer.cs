ffe13125e756674224641161a3f27b36a6eb43f5
Hey-CH <blacktomato0512@gmail.com>
2021-05-03 11:16:09 +0900

    YouTubeの動画をダウンロードしたり、MP3やGIFに変換するツール

---
+++ YoutubeDonwloader/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace YoutubeDonwloader {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(359, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(58, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(58, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(447, 19);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ffmpeg";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(259, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "GIF";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(307, 69);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 16);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "MP3";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(98, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 19);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxN_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "sec";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(176, 67);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(43, 19);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxN_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "sec";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 100);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Youtube Video Downloader(Converter)";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ YoutubeDonwloader/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using VideoLibrary;
+
+/*
+ * VideoLibraryを使用する場合、NuGetで入れた後
+ * ツール＞NuGetパッケージマネージャー＞パッケージマネージャーコンソールで
+ * PM> Update-Package -Reinstall
+ * を実行すると良いと思います。
+ * 
+ * System.Net.Httpでエラーが出る場合
+ * App.configを開いてSystem.Net.Httpのバージョンを4.0.0.0に設定する事
+ */
+
+namespace YoutubeDonwloader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            textBox2.Text = Properties.Settings.Default.ffmpegPath;
+            TextBoxCheck();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            Properties.Settings.Default.ffmpegPath = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            TextBoxCheck();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            TextBoxCheck();
+        }
+        private void TextBoxCheck() {
+            if (File.Exists(textBox2.Text) && textBox2.Text.EndsWith("ffmpeg.exe")) {
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                button1.Text = "Convert";
+            } else {
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                button1.Text = "Download";
+            }
+        }
+        private void TextBoxN_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e) {
+            //0～9と、バックスペース以外の時は、イベントをキャンセルする
+            if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b') {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //Youtube動画出力
+            var youTube = YouTube.Default;
+            var video = youTube.GetVideo(textBox1.Text);
+            System.IO.File.WriteAllBytes(video.FullName, video.GetBytes());
+
+            //出力
+            string interval = "";
+            TimeSpan start_position = new TimeSpan(0, 0, int.Parse(textBox3.Text));
+            try {
+                TimeSpan end_position = new TimeSpan(0, 0, int.Parse(textBox4.Text));
+                interval = (end_position - start_position).TotalSeconds.ToString();
+            } catch (Exception ex) {
+            }
+
+            if (checkBox1.Checked == true) {
+                string filePath = video.FullName + ".gif";
+                if (string.IsNullOrEmpty(interval)) {
+                    var arg = "-ss " + start_position.TotalSeconds.ToString()
+                        + " -i \"" + video.FullName + "\""
+                        + " \"" + filePath + "\"";
+                    System.Diagnostics.Process p = System.Diagnostics.Process.Start(textBox2.Text , arg);
+                    p.WaitForExit();
+                } else {
+                    var arg = "-ss " + start_position.TotalSeconds.ToString()
+                        + " -i \"" + video.FullName + "\""
+                        + " -t " + interval
+                        + " \"" + filePath + "\"";
+                    System.Diagnostics.Process p =
+                    System.Diagnostics.Process.Start(
+                        textBox2.Text
+                        , arg);
+                    p.WaitForExit();
+                }
+            }
+            if (checkBox2.Checked == true) {
+                string filePath = video.FullName+".mp3";
+                if (string.IsNullOrEmpty(interval)) {
+                    System.Diagnostics.Process p =
+                    System.Diagnostics.Process.Start(
+                        textBox2.Text
+                        , "-ss " + start_position.TotalSeconds.ToString()
+                        + " -i \"" + video.FullName + "\""
+                        + " \"" + filePath + "\"");
+                    p.WaitForExit();
+                } else {
+                    System.Diagnostics.Process p =
+                    System.Diagnostics.Process.Start(
+                        textBox2.Text
+                        , "-ss " + start_position.TotalSeconds.ToString()
+                        + " -i \"" + video.FullName + "\""
+                        + " -t " + interval 
+                        + " \"" + filePath + "\"");
+                    p.WaitForExit();
+                }
+            }
+
+            //完了
+            MessageBox.Show("完了しました。");
+        }
+    }
+}
